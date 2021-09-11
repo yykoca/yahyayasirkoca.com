4eef22dbbe99133d88ddda9fae7af0f8c7828212
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-09-11 23:51:31 +0200

    Contact component styled

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
-import '../App.css';
 
 function Contact() {
     const submitContactForm = (e) => {
@@ -9,29 +8,30 @@ function Contact() {
     }
 
     return (
-        <Container className="d-flex justify-content-center mt-5">
-            <Form onSubmit={submitContactForm} className="contact p-5 d-flex flex-column" style={{ border: "1px solid", borderRadius: "0px", boxShadow: "4px 4px 5px black" }}>
+        <Container fluid className="d-flex justify-content-end p-0 m-0" style={{ width: "100%" }}>
+            <Form onSubmit={submitContactForm} className="contact m-0 p-5 d-flex flex-column" style={{ width: "50vw", height: "85vh", background: "rgb(194,195,196" }}>
                 <h2 className="text-center mb-5">Contact me</h2>
-                <Form.Group className="mb-3" controlId="formBasicName">
+                
+                <Form.Group className="mb-3 px-5" controlId="formBasicName">
                     <Form.Control type="text" placeholder="What's your name?" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                <Form.Group className="mb-3 px-5" controlId="formBasicEmail">
                     <Form.Control type="email" placeholder="What's your email adress?" />
                     <Form.Text className="text-muted">
                         I'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicTextarea">
+                <Form.Group className="mb-3 px-5" controlId="formBasicTextarea">
                     <Form.Control as="textarea" placeholder="Leave a comment here" style={{ height: '100px' }} />
                 </Form.Group>
 
-
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Group className="mb-3 px-5" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
+                <Button variant="dark" type="submit" className="mx-5">
                     Let me know
                 </Button>
             </Form>
